51b1a83316b128af3b306a692834354677687e9c
Thao <hosithao1622004@gmail.com>
2025-03-17 14:44:54 +0700

    fix cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,12 +9,12 @@ dotenv.config();
 const app: Application = express();
 const port: number = parseInt(process.env.PORT as string, 10) || 8888;
 const hostname: string = process.env.HOST_NAME as string;
-// const corOptions: cors.CorsOptions = {
-//   origin: "http://localhost:8080",
-// };
+const corOptions: cors.CorsOptions = {
+  origin: "*",
+};
 
 // Middleware
-app.use(cors());
+app.use(cors(corOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
