f26d584a0441b2c5d699717ffebc29d318082946
Huy Thanh <hothanhhuy24062004@gmail.com>
2025-03-31 16:46:23 +0700

    add popup avatar

--- frontend/src/layouts/DefaultHeader/index.js
+++ frontend/src/layouts/DefaultHeader/index.js
@@ -2,6 +2,22 @@ import SearchInput from '@/components/SearchInput';
 import UserMenu from '@/components/UserMenu';
 
 const DefaultHeader = () => {
+  const [active, setActive] = useState(false);
+  const avatarRef = useRef(null);
+
+  // Xử lý click bên ngoài để ẩn popup
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (avatarRef.current && !avatarRef.current.contains(event.target)) {
+        setActive(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className='sticky top-0 z-50 w-full bg-white h-20 flex items-center justify-center'>
       <div className='flex w-3/4 justify-center'>
