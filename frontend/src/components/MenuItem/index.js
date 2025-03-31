04fd66825c982b908a0b2b99b5d1a3e91139461b
Huy Thanh <hothanhhuy24062004@gmail.com>
2025-03-31 16:46:23 +0700

    add popup avatar

--- frontend/src/components/MenuItem/index.js
+++ frontend/src/components/MenuItem/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
-const MenuItem = ({ icon, title, path }) => {
+const MenuItem = ({ icon, title, path, textSize = 'text-base', height }) => {
     const location = useLocation(); 
     const isActive = location.pathname === path; 
 
     return (
         <Link
             className={
-                'flex items-center text-base gap-4 my-2 p-3 rounded-md cursor-pointer hover:bg-slate-200'
+                `flex items-center ${textSize} ${height} gap-4 my-2 p-3 rounded-md cursor-pointer hover:bg-slate-200`
             }
             to={path || '#'}
         >
