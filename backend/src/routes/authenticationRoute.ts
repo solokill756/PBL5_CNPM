f01c438b28cdb2f17e0905bb8a73acdbb2927794
Thao <hosithao1622004@gmail.com>
2025-04-03 23:30:55 +0700

    Add email_send field to authentication model and update related services

--- backend/src/routes/authenticationRoute.ts
+++ backend/src/routes/authenticationRoute.ts
@@ -8,8 +8,8 @@ authRoutes.post("/login", (req: Request, res: Response) => login(req, res));
 authRoutes.post("/register", (req: Request, res: Response) => register(req, res));
 
 authRoutes.get("/logout", (req: Request, res: Response) => logout(req, res));
-authRoutes.get("/checkEmail" , (req : Request , res : Response) => checkEmailSignUp(req , res));
-authRoutes.get('/sendOtp' , (req : Request , res : Response) => sendOtp(req , res));
+authRoutes.post("/checkEmail" , (req : Request , res : Response) => checkEmailSignUp(req , res));
+authRoutes.post('/sendOtp' , (req : Request , res : Response) => sendOtp(req , res));
 authRoutes.post("/verifyOtp" , (req : Request , res : Response) => verifyOtp(req , res));
 authRoutes.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
 authRoutes.get("/google/callback" , passport.authenticate("google") , (req : Request, res : Response) => {
