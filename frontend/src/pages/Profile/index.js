6fa414f50afa8bf81461cecc8d095f64d71bc104
volequyen <qvo9110@gmail.com>
2025-04-14 12:51:39 +0700

    change file position

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import DefaultHeader from "@/layouts/DefaultHeader";
+import Card from '@/components/Card/PersonalCard';
+import InterfaceCard from '@/components/Card/InterfaceCard';
+import Privacy from '@/components/Card/Privacy';
 
 const Profile = () => {
     return (
-        <div className="text-center">
-            <h1 className="text-2xl font-bold">Profile Page</h1>
-            <p>User Profile Information</p>
-        </div>
+        <main className='min-h-screen flex flex-col items-center'>
+            <DefaultHeader />
+            <div className="flex flex-col items-center w-full mt-4">
+                <div className="w-full max-w-4xl px-4">
+                    <span className='text-base font-semibold text-gray-600 dark:text-white mb-4'>Thông tin cá nhân</span>
+                        <Card />
+                </div>
+                <div className="w-full max-w-4xl px-4">
+                    <span className='text-base font-semibold text-gray-600 dark:text-white mb-4'>Giao diện</span>
+                        <InterfaceCard />
+                </div>
+                <div className="w-full max-w-4xl px-4">
+                    <span className='text-base font-semibold text-gray-600 dark:text-white mb-4'>Tài khoản và quyền riêng tư</span>
+                        <Privacy />
+                </div>
+            </div>
+        </main>
     );
 };
 
