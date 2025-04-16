5fda197b74b2d623215355ecfd069e9f9925d5df
volequyen <qvo9110@gmail.com>
2025-04-16 23:54:44 +0700

    completed profile ui

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -3,6 +3,7 @@ import DefaultHeader from "@/layouts/DefaultHeader";
 import Card from '@/components/Card/PersonalCard';
 import InterfaceCard from '@/components/Card/InterfaceCard';
 import Privacy from '@/components/Card/Privacy';
+import NotificationCard from '@/components/Card/NotificationCard';
 
 const Profile = () => {
     return (
@@ -14,13 +15,14 @@ const Profile = () => {
                         <Card />
                 </div>
                 <div className="w-full max-w-4xl px-4">
-                    <span className='text-base font-semibold text-gray-600 dark:text-white mb-4'>Giao diện</span>
-                        <InterfaceCard />
+                    <span className='text-base font-semibold text-gray-600 dark:text-white mb-4'>Thông báo</span>
+                        <NotificationCard />
                 </div>
                 <div className="w-full max-w-4xl px-4">
                     <span className='text-base font-semibold text-gray-600 dark:text-white mb-4'>Tài khoản và quyền riêng tư</span>
                         <Privacy />
                 </div>
+                
             </div>
         </main>
     );
