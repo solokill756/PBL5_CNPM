167484bedb533112dbb927cca2da4ed286140300
Huy Thanh <hothanhhuy24062004@gmail.com>
2025-04-22 20:13:49 +0700

    Reapply "completed fetch homepage api"
    This reverts commit 11de69fd1e5c42c87285a2781c456ef24e97ec64.

---
+++ frontend/src/api/topAuthor.js
@@ -0,0 +1,9 @@
+export const fetchTopAuthor = async (axiosPrivate) => {
+  try {
+    const res = await axiosPrivate.get("/api/homePage/topAuthor");
+
+    if (res.data) return { data: res.data, error: null };
+  } catch (error) {
+    return { data: null, error: error.response?.data?.error };
+  }
+};
