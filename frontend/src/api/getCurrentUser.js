0ee3b7149e0cec0f2ab2e346b0e22c075f5b6ad3
Thao <hosithao1622004@gmail.com>
2025-05-02 16:09:52 +0700

    refactor: remove frontend components, assets, and configurations to clean up the project structure

--- frontend/src/api/getCurrentUser.js
+++
@@ -1,8 +0,0 @@
-export const fetchCurrentUser = async () => {
-  try {
-    const res = await axiosPrivate.get("http://localhost:9000/api/auth/user");
-    return { accessToken, refreshToken, user } = res.data.user;
-  } catch (error) {
-    console.log(error);
-  }
-};
