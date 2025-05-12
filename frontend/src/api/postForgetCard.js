6c7faf924f9cac672497cfe744d40a867b87c55c
Huy Thanh <hothanhhuy24062004@gmail.com>
2025-05-12 20:15:28 +0700

    Enhance flashcard functionality by adding list_id parameter to postForgetCard and postRememberCard API calls.

--- frontend/src/api/postForgetCard.js
+++ frontend/src/api/postForgetCard.js
@@ -1,7 +1,8 @@
-export const postForgetCard = async (axios, card_id = []) => {
+export const postForgetCard = async (axios, card_id = [], list_id) => {
     try {
         const response = await axios.post(`/api/learn/not-remenber-flashcard`, {
-            flashcard_ids: card_id
+            flashcard_ids: card_id,
+            list_id: list_id
         });
         return response.data.data;
     } catch (error) {
